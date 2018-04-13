00e3ba4b79a599a49ec13dde7280597471c7e206
mikelt1 <38344252+mikelt1@users.noreply.github.com>
2018-04-13 10:15:11 +0200

    Create botonesSuma.java

---
+++ botonesSuma.java
@@ -0,0 +1,14 @@
+public class Modelo {
+	public int Hecho (int resultado){
+		resultado=resultado+2;
+		return (resultado);
+	}
+	public int Intentado (int resultado){
+		resultado=resultado+1;
+		return (resultado);
+	}
+	public int noHecho (int resultado){
+		resultado=resultado+0;
+		return (resultado);
+	}
+}
