28ea4403ba3e41095e73cf84104c70a6907d712c
GARFIELD <30346465+aimarperuchena@users.noreply.github.com>
2018-04-19 09:28:33 +0200

    Update ClaseModelo.java

--- ClaseModelo.java
+++ ClaseModelo.java
@@ -1,23 +1,24 @@
 import java.util.Scanner;
+
 public class ClaseModelo {
-public static int  cuantosJugadores(){
+
+	public static int  cuantosJugadores(){
 		Scanner sc= new Scanner(System.in);
 		int totaljugadores;
-		
+
 		totaljugadores=sc.nextInt();
-		
+
 		return totaljugadores;
 	}
-	
-	public static String [] recogerNombres(int totaljugadores){
+
+	public static String [] nombresjugadores(int totaljugadores){
 		Scanner sc=new Scanner(System.in);
 		String nombres[];
 		nombres=new String[totaljugadores];
+
 		
-		for(int i=0; i< nombres.length; i++){
-			nombres[i]=sc.nextLine();
-		}
-		
+
 		return nombres;
 	}
+
 }
